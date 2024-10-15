12cdd2d804264699ba46a2e8db5821b1d669e6ff
HOUDEYER Lucie <102391460+HdrLucie@users.noreply.github.com>
2024-10-15 12:44:58 +0200

    Create source.c

---
+++ level2/source.c
@@ -0,0 +1,22 @@
+void p(void)
+{
+  uint unaff_retaddr;
+  char local_50 [76];
+  
+  fflush(stdout);
+  gets(local_50);
+  if ((unaff_retaddr & 0xb0000000) == 0xb0000000) {
+    printf("(%p)\n",unaff_retaddr);
+                    /* WARNING: Subroutine does not return */
+    _exit(1);
+  }
+  puts(local_50);
+  strdup(local_50);
+  return;
+}
+
+void main(void)
+{
+  p();
+  return;
+}
