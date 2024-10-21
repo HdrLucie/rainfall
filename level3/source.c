99c5b2b6cfde730df69fc78124f4d0e29948f7fd
HOUDEYER Lucie <102391460+HdrLucie@users.noreply.github.com>
2024-10-21 17:53:57 +0200

    Update source.c

--- level3/source.c
+++ level3/source.c
@@ -3,7 +3,7 @@ void v(void)
   char buffer [520];
   fgets(buffer,512,stdin);
   printf(buffer);
-  if (m == 64) {
+  if (*(int *)0x804988c == 64) {
     fwrite("Wait what?!\n",1,12,stdout);
     system("/bin/sh");
   }
